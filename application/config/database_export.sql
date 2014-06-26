252bdd685eb5bbeb3d03c2353438366ca9bd255c
Neal Howarth <contact@how-media.co.uk>
2014-06-26 12:33:41 +0100

    re-upload

--- application/config/database_export.sql
+++ application/config/database_export.sql
@@ -1,18 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 19, 2014 at 11:38 PM
--- Server version: 5.5.25
--- PHP Version: 5.4.4
+-- Generation Time: Jun 26, 2014 at 11:29 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
 -- Database: `appt`
 --
+CREATE DATABASE IF NOT EXISTS `appt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `appt`;
 
 -- --------------------------------------------------------
 
@@ -20,7 +22,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `appointments`
 --
 
-CREATE TABLE `appointments` (
+CREATE TABLE IF NOT EXISTS `appointments` (
   `id` int(30) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `staff_id` int(10) NOT NULL,
@@ -34,17 +36,19 @@ CREATE TABLE `appointments` (
   `deposit_amount` decimal(8,2) NOT NULL,
   `status` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `appointments`
 --
 
 INSERT INTO `appointments` (`id`, `user_id`, `staff_id`, `client_id`, `service_id`, `date`, `start_time`, `end_time`, `service_name`, `price`, `deposit_amount`, `status`) VALUES
-(2, 9, 1, 3, 5, '2014-03-19', '09:00:00', '10:30:00', 'Full Set Acrylic Nails', 27.50, 0.00, 'active'),
-(3, 9, 1, 3, 5, '2014-03-19', '13:00:00', '14:30:00', 'Full Set Acrylic Nails', 27.50, 0.00, 'active'),
-(4, 9, 1, 3, 5, '2014-03-17', '10:30:00', '12:00:00', 'Full Set Acrylic Nails', 27.50, 0.00, 'active'),
-(5, 9, 1, 5, 6, '2014-03-19', '15:45:00', '16:45:00', 'Full Body Massage', 35.00, 20.00, 'active');
+(2, 9, 1, 3, 5, '2014-03-20', '09:00:00', '10:30:00', 'Full Set Acrylic Nails', '27.50', '0.00', 'active'),
+(3, 9, 1, 3, 5, '2014-03-20', '13:00:00', '14:30:00', 'Full Set Acrylic Nails', '27.50', '0.00', 'active'),
+(4, 9, 1, 3, 5, '2014-03-20', '10:30:00', '12:00:00', 'Full Set Acrylic Nails', '27.50', '0.00', 'active'),
+(5, 9, 1, 5, 6, '2014-03-20', '15:45:00', '16:45:00', 'Full Body Massage', '35.00', '20.00', 'active'),
+(6, 9, 4, 4, 10, '2014-03-20', '14:30:00', '14:36:00', '6 minutes', '3.50', '3.50', 'paid'),
+(7, 9, 3, 4, 5, '2014-06-05', '10:15:00', '11:45:00', 'Full Set Acrylic Nails', '27.50', '10.00', 'active');
 
 -- --------------------------------------------------------
 
@@ -52,7 +56,7 @@ INSERT INTO `appointments` (`id`, `user_id`, `staff_id`, `client_id`, `service_i
 -- Table structure for table `blog_categories`
 --
 
-CREATE TABLE `blog_categories` (
+CREATE TABLE IF NOT EXISTS `blog_categories` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
@@ -73,7 +77,7 @@ INSERT INTO `blog_categories` (`id`, `name`, `slug`, `order`) VALUES
 -- Table structure for table `blog_comment_replies`
 --
 
-CREATE TABLE `blog_comment_replies` (
+CREATE TABLE IF NOT EXISTS `blog_comment_replies` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `blog_comment_id` int(6) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -91,7 +95,7 @@ CREATE TABLE `blog_comment_replies` (
 -- Table structure for table `blog_posts`
 --
 
-CREATE TABLE `blog_posts` (
+CREATE TABLE IF NOT EXISTS `blog_posts` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
@@ -119,7 +123,7 @@ INSERT INTO `blog_posts` (`id`, `title`, `slug`, `summary`, `content`, `image`, 
 -- Table structure for table `blog_post_comments`
 --
 
-CREATE TABLE `blog_post_comments` (
+CREATE TABLE IF NOT EXISTS `blog_post_comments` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `blog_post_id` int(6) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -137,7 +141,7 @@ CREATE TABLE `blog_post_comments` (
 -- Table structure for table `business_categories`
 --
 
-CREATE TABLE `business_categories` (
+CREATE TABLE IF NOT EXISTS `business_categories` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -161,7 +165,7 @@ INSERT INTO `business_categories` (`id`, `name`) VALUES
 -- Table structure for table `business_details`
 --
 
-CREATE TABLE `business_details` (
+CREATE TABLE IF NOT EXISTS `business_details` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `street` varchar(255) DEFAULT NULL,
@@ -191,7 +195,7 @@ INSERT INTO `business_details` (`id`, `user_id`, `street`, `town`, `county`, `po
 -- Table structure for table `business_hours`
 --
 
-CREATE TABLE `business_hours` (
+CREATE TABLE IF NOT EXISTS `business_hours` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `mon_open` time NOT NULL,
@@ -224,7 +228,7 @@ INSERT INTO `business_hours` (`id`, `user_id`, `mon_open`, `mon_close`, `tues_op
 -- Table structure for table `clients`
 --
 
-CREATE TABLE `clients` (
+CREATE TABLE IF NOT EXISTS `clients` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `first_name` varchar(100) NOT NULL,
@@ -256,7 +260,7 @@ INSERT INTO `clients` (`id`, `user_id`, `first_name`, `last_name`, `telephone`, 
 -- Table structure for table `client_notes`
 --
 
-CREATE TABLE `client_notes` (
+CREATE TABLE IF NOT EXISTS `client_notes` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `client_id` int(10) NOT NULL,
   `staff_id` int(10) NOT NULL,
@@ -281,7 +285,7 @@ INSERT INTO `client_notes` (`id`, `client_id`, `staff_id`, `note`, `created_at`)
 -- Table structure for table `countries`
 --
 
-CREATE TABLE `countries` (
+CREATE TABLE IF NOT EXISTS `countries` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `currency_symbol` varchar(5) NOT NULL,
@@ -302,7 +306,7 @@ INSERT INTO `countries` (`id`, `name`, `currency_symbol`, `currency_html`) VALUE
 -- Table structure for table `email_settings`
 --
 
-CREATE TABLE `email_settings` (
+CREATE TABLE IF NOT EXISTS `email_settings` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `email_reminders` int(1) NOT NULL DEFAULT '0',
@@ -324,7 +328,7 @@ INSERT INTO `email_settings` (`id`, `user_id`, `email_reminders`, `hours_before`
 -- Table structure for table `plans`
 --
 
-CREATE TABLE `plans` (
+CREATE TABLE IF NOT EXISTS `plans` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `details` text NOT NULL,
@@ -341,7 +345,7 @@ CREATE TABLE `plans` (
 --
 
 INSERT INTO `plans` (`id`, `name`, `details`, `price`, `interval_length`, `interval_unit`, `free_days`, `is_active`) VALUES
-(1, 'Appt Test Plan', 'Appt test plan, 19.99 per month, starts after 30 days', 19.99, 1, 'month', NULL, 1);
+(1, 'Appt Test Plan', 'Appt test plan, 19.99 per month, starts after 30 days', '19.99', 1, 'month', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -349,7 +353,7 @@ INSERT INTO `plans` (`id`, `name`, `details`, `price`, `interval_length`, `inter
 -- Table structure for table `services`
 --
 
-CREATE TABLE `services` (
+CREATE TABLE IF NOT EXISTS `services` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `service_category_id` int(6) NOT NULL,
@@ -366,12 +370,12 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `user_id`, `service_category_id`, `name`, `details`, `cost`, `sell`, `duration`) VALUES
-(1, 9, 1, 'Back Massage', 'Luxury back massage treatment using the finest oils', 12.00, 25.99, 30),
-(5, 9, 2, 'Full Set Acrylic Nails', 'Full Set Acrylic Nails', 15.00, 27.50, 90),
-(6, 9, 1, 'Full Body Massage', 'Full body massage', 12.00, 35.00, 60),
-(8, 9, 3, 'Test Service', 'This is a test service', 0.00, 2.99, 25),
-(9, 9, 4, '3 Minutes', '3 minutes on sunbed', 0.50, 2.50, 3),
-(10, 9, 4, '6 minutes', '6 minutes on sunbed', 0.80, 3.50, 6);
+(1, 9, 1, 'Back Massage', 'Luxury back massage treatment using the finest oils', '12.00', '25.99', 30),
+(5, 9, 2, 'Full Set Acrylic Nails', 'Full Set Acrylic Nails', '15.00', '27.50', 90),
+(6, 9, 1, 'Full Body Massage', 'Full body massage', '12.00', '35.00', 60),
+(8, 9, 3, 'Test Service', 'This is a test service', '0.00', '2.99', 25),
+(9, 9, 4, '3 Minutes', '3 minutes on sunbed', '0.50', '2.50', 3),
+(10, 9, 4, '6 minutes', '6 minutes on sunbed', '0.80', '3.50', 6);
 
 -- --------------------------------------------------------
 
@@ -379,7 +383,7 @@ INSERT INTO `services` (`id`, `user_id`, `service_category_id`, `name`, `details
 -- Table structure for table `service_categories`
 --
 
-CREATE TABLE `service_categories` (
+CREATE TABLE IF NOT EXISTS `service_categories` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -402,7 +406,7 @@ INSERT INTO `service_categories` (`id`, `user_id`, `name`) VALUES
 -- Table structure for table `staff`
 --
 
-CREATE TABLE `staff` (
+CREATE TABLE IF NOT EXISTS `staff` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `first_name` varchar(100) NOT NULL,
@@ -435,7 +439,7 @@ INSERT INTO `staff` (`id`, `user_id`, `first_name`, `last_name`, `email`, `passw
 -- Table structure for table `staff_hours`
 --
 
-CREATE TABLE `staff_hours` (
+CREATE TABLE IF NOT EXISTS `staff_hours` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `staff_id` int(10) NOT NULL,
   `mon_open` time NOT NULL,
@@ -470,7 +474,7 @@ INSERT INTO `staff_hours` (`id`, `staff_id`, `mon_open`, `mon_close`, `tues_open
 -- Table structure for table `staff_services`
 --
 
-CREATE TABLE `staff_services` (
+CREATE TABLE IF NOT EXISTS `staff_services` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `staff_id` int(10) NOT NULL,
   `service_id` int(10) NOT NULL,
@@ -497,7 +501,7 @@ INSERT INTO `staff_services` (`id`, `staff_id`, `service_id`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
